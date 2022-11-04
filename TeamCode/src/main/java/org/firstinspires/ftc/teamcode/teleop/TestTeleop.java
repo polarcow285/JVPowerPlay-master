5f524965f30b6b00f1fee4107e2c4ccc3341ec1a
cksakai <cksakai3000@gmail.com>
2022-11-03 20:02:58 -0700

    fixed bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestTeleop.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Projects.hi;
@@ -68,50 +73,46 @@ public class TestTeleop extends LinearOpMode {
 
                 robot.lift.setPower(0.25);
             }
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = -gamepad1.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        robot.fLeftWheel.setPower(frontLeftPower);
+        robot.bLeftWheel.setPower(backLeftPower);
+        robot.fRightWheel.setPower(frontRightPower);
+        robot.bRightWheel.setPower(backRightPower);
            // if(robot.lift.getTargetPosition()>200||robot.lift.getTargetPosition() < 0){
 
                 //robot.lift.setPower(0);
             //}
             //telemetry.addData("Lift Encoder Count", robot.lift.getCurrentPosition());
            // telemetry.update();
-/*
+
             // move the arms up and down
-            if (gamepad2.left_stick_y == true) {
-                robot.liftArmMotorThing.setPower(-gamepad2.left_stick_y);
+         //   if (gamepad2.left_stick_y == true) {
+              //  robot.liftArmMotorThing.setPower(-gamepad2.left_stick_y);
             }
 
             // moves the manipulator (manipulatorMotor)
-            if (gamepad2.a == true) {
-                robot.manipulatorMotor.setPower(.2);
-            }
-            if (gamepad2.y == true) {
-                robot.manipulatorMotor.setPower(-.2);
-            }
+    // if (gamepad2.a == true) {
+              //  robot.manipulatorMotor.setPower(.2);
+         //   }
+         //   if (gamepad2.y == true) {
+           //     robot.manipulatorMotor.setPower(-.2);
+          //  }
 
 
 
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            robot.fLeftWheel.setPower(frontLeftPower);
-            robot.bLeftWheel.setPower(backLeftPower);
-            robot.fRightWheel.setPower(frontRightPower);
-            robot.bRightWheel.setPower(backRightPower);
         }
-    }
-}
-//
 
-
-}
 
