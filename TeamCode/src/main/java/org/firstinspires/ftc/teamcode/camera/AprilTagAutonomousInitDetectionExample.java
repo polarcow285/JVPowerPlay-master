7d49cee246018490baf77a21c4070d5537f98717
Minty20090 <44348348+Minty20090@users.noreply.github.com>
2022-11-29 18:51:40 -0800

    Update AprilTagAutonomousInitDetectionExample.java
    fixed camera error(hopefully)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/camera/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/camera/AprilTagAutonomousInitDetectionExample.java
@@ -63,6 +63,7 @@ public class AprilTagAutonomousInitDetectionExample<tagOfInterest> extends Linea
     @Override
     public void runOpMode()
     {
+        robot.init(hardwareMap);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
